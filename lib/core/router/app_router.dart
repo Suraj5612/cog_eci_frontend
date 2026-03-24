f24c5dd6fc157082a5183a55c46faf7e658468f3
Suraj Kumar Jena <suraj.jena@cognecto.com>
2026-03-24 17:08:33 +0530

    added file routes

---
+++ lib/core/router/app_router.dart
@@ -0,0 +1,17 @@
+import 'package:go_router/go_router.dart';
+
+import '../../features/auth/screens/login_screen.dart';
+import '../../features/auth/screens/splash_screen.dart';
+import '../../features/home/screens/home_screen.dart';
+
+final GoRouter appRouter = GoRouter(
+  initialLocation: '/',
+
+  routes: [
+    GoRoute(path: '/', builder: (context, state) => const SplashScreen()),
+
+    GoRoute(path: '/login', builder: (context, state) => const LoginScreen()),
+
+    GoRoute(path: '/home', builder: (context, state) => const HomeScreen()),
+  ],
+);
