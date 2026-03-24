2c503b6cd4f32c39b4577bfd406ff7ed14f95739
Suraj Kumar Jena <suraj.jena@cognecto.com>
2026-03-25 00:01:26 +0530

    added token for auth

--- lib/core/services/api_base_helper.dart
+++ lib/core/services/api_base_helper.dart
@@ -3,34 +3,37 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
 
 import '../exceptions/api_exception.dart';
+import 'storage_service.dart';
 
 class ApiBaseHelper {
   static String get baseUrl => dotenv.env['BASE_URL']!;
 
-  static Future<dynamic> get(String endpoint, {String? token}) async {
+  static Future<dynamic> get(String endpoint) async {
+    final headers = await _headers();
+
     final response = await http.get(
       Uri.parse('$baseUrl$endpoint'),
-      headers: _headers(token),
+      headers: headers,
     );
 
     return _handleResponse(response);
   }
 
-  static Future<dynamic> post(
-    String endpoint,
-    dynamic body, {
-    String? token,
-  }) async {
+  static Future<dynamic> post(String endpoint, dynamic body) async {
+    final headers = await _headers();
+
     final response = await http.post(
       Uri.parse('$baseUrl$endpoint'),
-      headers: _headers(token),
+      headers: headers,
       body: jsonEncode(body),
     );
 
     return _handleResponse(response);
   }
 
-  static Map<String, String> _headers(String? token) {
+  static Future<Map<String, String>> _headers() async {
+    final token = await StorageService.getToken();
+
     return {
       'Content-Type': 'application/json',
       if (token != null) 'Authorization': 'Bearer $token',
