e7f0e81c067767e5cafdc9fc501525f7abac87c5
Suraj Kumar Jena <suraj.jena@cognecto.com>
2026-03-24 17:08:02 +0530

    app constants

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Color(0xFF00A481);
+  static const secondary = Color(0xFFFFFEFF);
+
+  static const textPrimary = Color(0xFF161718);
+  static const textSecondary = Color(0xFFBEBFBF);
+}
