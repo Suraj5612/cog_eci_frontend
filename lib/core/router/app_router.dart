bccda1cb342e744a91a836bdee3f0754ec49ac4e
Suraj Kumar Jena <suraj.jena@cognecto.com>
2026-03-25 05:52:50 +0530

    router updated

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -3,6 +3,9 @@ import 'package:go_router/go_router.dart';
 import '../../features/auth/screens/login_screen.dart';
 import '../../features/auth/screens/splash_screen.dart';
 import '../../features/home/screens/home_screen.dart';
+import '../../features/ocr/screens/image_preview_screen.dart';
+import '../../features/voter/screens/voter_details_screen.dart';
+import '../../features/voter/screens/voter_list_screen.dart';
 
 final GoRouter appRouter = GoRouter(
   initialLocation: '/',
@@ -13,5 +16,30 @@ final GoRouter appRouter = GoRouter(
     GoRoute(path: '/login', builder: (context, state) => const LoginScreen()),
 
     GoRoute(path: '/home', builder: (context, state) => const HomeScreen()),
+
+    GoRoute(
+      path: '/preview',
+      builder: (context, state) {
+        final imagePath = state.extra as String;
+        return ImagePreviewScreen(imagePath: imagePath);
+      },
+    ),
+
+    GoRoute(
+      path: '/voter-details',
+      builder: (context, state) {
+        final extra = state.extra as Map<String, dynamic>;
+
+        return VoterDetailsScreen(
+          imagePath: extra['imagePath'],
+          ocrData: extra['data'],
+        );
+      },
+    ),
+
+    GoRoute(
+      path: '/voters',
+      builder: (context, state) => const VoterListScreen(),
+    ),
   ],
 );
