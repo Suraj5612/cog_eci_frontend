9385340c31e63222566c0dc7b2a5e05e40b307a6
Suraj Kumar Jena <suraj.jena@cognecto.com>
2026-03-25 05:55:17 +0530

    updated main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,16 @@
+import 'package:cog_eci_frontend/features/auth/bloc/auth_event.dart';
+import 'package:cog_eci_frontend/features/auth/repository/auth_repository.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'core/router/app_router.dart';
+import 'features/auth/bloc/auth_bloc.dart';
+import 'features/ocr/bloc/ocr_bloc.dart';
+import 'features/voter/bloc/voter_bloc.dart';
+import 'features/voter/bloc/voter_repository.dart';
 
-void main() {
+void main() async {
+  await dotenv.load(fileName: ".env");
   runApp(const MyApp());
 }
 
@@ -10,6 +19,18 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(routerConfig: appRouter);
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<AuthBloc>(
+          create: (_) => AuthBloc(AuthRepository())..add(CheckAuthStatus()),
+        ),
+        BlocProvider<OCRBloc>(create: (_) => OCRBloc()),
+        BlocProvider<VoterBloc>(create: (_) => VoterBloc(VoterRepository())),
+      ],
+      child: MaterialApp.router(
+        debugShowCheckedModeBanner: false,
+        routerConfig: appRouter,
+      ),
+    );
   }
 }
